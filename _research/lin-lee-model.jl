ac231bd8aee9c141037780bcc4d67f69aa39c9b7
Oisin Fitzgerald <fitzgeraldoisin@gmail.com>
2020-11-20 16:40:26 +1100

    clean

--- _research/lin-lee-model.jl
+++
@@ -1,9 +0,0 @@
-using Plots
-using DifferentialEquations
-
-# Insulin glucose model from Lin, Lee et al (2008)
-lin! = @ode_def LinLee begin
-    dG = -pg*G - Si*(G + Ge)*(Q/(1 + αg*Q)) + P
-    dQ = -k*Q + k*I
-    dI = -n*(I/(1 + αi*I)) + uex/Vi
-end G Q I
